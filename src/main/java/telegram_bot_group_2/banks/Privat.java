30b25d4b393d283dbd8973525181b5764b2189ff
ButenkoIvan <butenkoivan898@gmai.com>
2022-09-19 21:50:08 +0300

    Started the work

--- src/main/java/telegram_bot_group_2/banks/Privat.java
+++ src/main/java/telegram_bot_group_2/banks/Privat.java
@@ -1,4 +1,34 @@
 package telegram_bot_group_2.banks;
 
+import java.util.Objects;
+
 public class Privat {
+    private String ccy;
+    private String base_ccy;
+    private float buy;
+    private float sale;
+    public String getCcy() { return ccy; }
+    public String getBase_ccy() { return base_ccy; }
+    public float getBuy() { return buy; }
+    public float getSale() { return sale; }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Privat privat = (Privat) o;
+        return Float.compare(privat.buy, buy) == 0 && Float.compare(privat.sale, sale) == 0 && ccy.equals(privat.ccy) && base_ccy.equals(privat.base_ccy);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(ccy, base_ccy, buy, sale);
+    }
+    @Override
+    public String toString() {
+        return "Privat{" +
+                "ccy='" + ccy + '\'' +
+                ", base_ccy='" + base_ccy + '\'' +
+                ", buy=" + buy +
+                ", sale=" + sale +
+                '}';
+    }
 }
