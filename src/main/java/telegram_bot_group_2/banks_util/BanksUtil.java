8f0ba4eb0cdb51983bfcb836e8851bfc06aa1348
ButenkoIvan <butenkoivan898@gmai.com>
2022-09-20 09:06:33 +0300

    Test

--- src/main/java/telegram_bot_group_2/banks_util/BanksUtil.java
+++ src/main/java/telegram_bot_group_2/banks_util/BanksUtil.java
@@ -3,6 +3,7 @@ package telegram_bot_group_2.banks_util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import telegram_bot_group_2.banks.NBU;
+import telegram_bot_group_2.banks.Privat;
 
 
 import java.lang.reflect.Type;
@@ -16,9 +17,10 @@ public class BanksUtil {
 
     private static final String NBU_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?json";
     private static final String MONOBANK_URL = "https://api.monobank.ua/bank/currency";
-//    private static final String PRIVAT_URL = "";
+    private static final String PRIVAT_URL = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=11";
 
     static Type typeNbu = new TypeToken<List<NBU>> (){}.getType();
+    static Type typePrivat = new TypeToken<List<Privat>>() { }.getType();
 
 
 }
